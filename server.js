eed94fffa1f7a5f6eb454a8f9ab77dd4bc6cb7b2
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2017-04-18 17:40:45 -0500

    FOO
    FOO

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿"use strict;"
+
+const http = require('http');
+const PORT = process.env.port || 1337;
+const ecstatic = require('ecstatic');
+
+http.createServer(
+    ecstatic({ root: __dirname + '/public' })
+).listen(PORT);
+
